b60385f0fdfbe445eb3eaaec6ceb457eccebca34
root <root@shashiudawa4.mylabserver.com>
2017-12-12 01:00:50 +0000

    final

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,13 @@
 class nginx::params {
-  $ensure = 'present'
-  $package_name = 'nginx'
-  $config_dir = '/etc/nginx'
-  $config_ensure = 'file'
-  $config_mode = '0664'
-  $config_owner = 'root'
-  $config_group = 'root'
-  $config_confd = "${config_dir}/conf.d"
-  $config_log_dir = '/var/log/nginx'
+  $ensure          = 'present'
+  $package_name    = 'nginx'
+  $config_dir      = '/etc/nginx'
+  $config_ensure   = 'file'
+  $config_mode     = '0664'
+  $config_owner    = 'root'
+  $config_group    = 'root'
+  $config_confd    = "${config_dir}/conf.d"
+  $config_log_dir  = '/var/log/nginx'
   $config_pid_file = '/run/nginx.pid'
 
   $config_vdir_enable = $facts['os']['family'] ? {
@@ -25,11 +25,11 @@ class nginx::params {
     default  => $config_confd,
   }
 
-  $service_name = 'nginx'
-  $service_ensure = 'running'
-  $service_pattern = 'nginx'
-  $service_enable = true
-  $service_hasstatus = true
+  $service_name       = 'nginx'
+  $service_ensure     = 'running'
+  $service_pattern    = 'nginx'
+  $service_enable     = true
+  $service_hasstatus  = true
   $service_hasrestart = true
   
   $docroot = '/usr/share/nginx'
